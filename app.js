1da3d4efb1f73ea78ead90868a2712e0db928906
Eugenio Marino <geggiodev@Air-di-Eugenio.homenet.telecomitalia.it>
2021-11-05 12:00:52 +0100

    Adding More JS

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+const items = document.querySelectorAll(".item");
+console.log(items);
+items.forEach((item) => {
+  item.addEventListener("click", function () {
+    const target = item.getAttribute("data-target");
+    const id = "faq_" + target;
+
+    let showBox = document.getElementById(id);
+    const boxes = document.querySelectorAll(".big-section-container");
+    boxes.forEach(box => {
+      box.style.display = "none";
+    });
+
+    showBox.style.display = "block";
+    // console.log(showBox);
+  });
+});
+
 const titles = document.querySelectorAll(".faq-title");
 
 titles.forEach((title) => {
